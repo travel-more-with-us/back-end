defcaf407d794c5058046fdbb5bddd6317bbe56b
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-06-28 10:36:26 +0300

    Added models to TravelMore app

--- TravelMore/admin.py
+++ TravelMore/admin.py
@@ -1,3 +1,167 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from TravelMore.models import (
+    Destination,
+    Stay,
+    StayFrames,
+    Accommodation,
+    AccommodationFrames,
+    RatingStar,
+    ReviewStay,
+    ReviewDestination,
+    Amenity,
+    RatingStay,
+    RatingDestination,
+    Booking,
+)
+
+
+class ReviewDestinationInline(admin.TabularInline):
+    model = ReviewDestination
+    extra = 1
+    readonly_fields = ("user", )
+
+
+@admin.register(Destination)
+class DestinationAdmin(admin.ModelAdmin):
+    list_display = ("name", "country",)
+    search_fields = ("name", "country")
+    inlines = [ReviewDestinationInline]
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(
+            f"<img src={obj.image.url} width='60' height='50'"
+        )
+
+    get_image.short_description = "Image"
+
+
+class ReviewStayInline(admin.TabularInline):
+    model = ReviewStay
+    extra = 1
+    readonly_fields = ("user", )
+
+
+class StayFramesInline(admin.TabularInline):
+    model = StayFrames
+    extra = 1
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(
+            f"<img src={obj.image.url} width='130' height='100'"
+        )
+
+    get_image.short_description = "Stay pictures"
+
+
+@admin.register(Stay)
+class StayAdmin(admin.ModelAdmin):
+    list_display = ("name", "address", "destination",)
+    search_fields = ("name", "country")
+    inlines = [StayFramesInline, ReviewStayInline]
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(
+            f"<img src={obj.image.url} width='60' height='50'"
+        )
+
+    get_image.short_description = "Image"
+
+
+@admin.register(StayFrames)
+class StayFramesAdmin(admin.ModelAdmin):
+    list_display = ("title", "stays", "get_image")
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(
+            f"<img src={obj.image.url} width='60' height='50'"
+        )
+
+    get_image.short_description = "Image"
+
+
+class AccommodationFramesInline(admin.TabularInline):
+    model = AccommodationFrames
+    extra = 1
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(
+            f"<img src={obj.image.url} width='130' height='100'"
+        )
+
+    get_image.short_description = "Stay pictures"
+
+
+@admin.register(Accommodation)
+class AccommodationAdmin(admin.ModelAdmin):
+    list_display = ("name", "type_room", "number_rooms", "number_beds", "get_image")
+    search_fields = ("name", "type_room")
+    inlines = [AccommodationFramesInline]
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(
+            f"<img src={obj.image.url} width='60' height='50'"
+        )
+
+    get_image.short_description = "Image"
+
+
+@admin.register(AccommodationFrames)
+class AccommodationFramesAdmin(admin.ModelAdmin):
+    list_display = ("title", "rooms", "get_image")
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(
+            f"<img src={obj.image.url} width='60' height='50'"
+        )
+
+    get_image.short_description = "Image"
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "stay",
+        "arrival_date",
+        "departure_date",
+        "number_of_guests",
+        "night_price",
+        "total_price"
+    )
+    readonly_fields = ("user",)
+
+
+@admin.register(ReviewStay)
+class ReviewStayAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "stay")
+    readonly_fields = ("user",)
+
+
+@admin.register(ReviewDestination)
+class ReviewDestinationAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "destination")
+    readonly_fields = ("user",)
+
+
+@admin.register(RatingStay)
+class RatingStayAdmin(admin.ModelAdmin):
+    list_display = ("stay", "star", "user")
+
+
+@admin.register(RatingDestination)
+class RatingDestinationAdmin(admin.ModelAdmin):
+    list_display = ("destination", "star", "user")
+
+
+admin.site.register(RatingStar)
+admin.site.register(Amenity)
--- TravelMore/models.py
+++ TravelMore/models.py
@@ -1,41 +1,253 @@
+import os
+import uuid
+
+from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Amenity(models.Model):
+    name = models.CharField(max_length=20, blank=True)
+
+    class Meta:
+        verbose_name = "amenity"
+        verbose_name_plural = "amenities"
+
+    def __str__(self):
+        return self.name
+
+
+def destination_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/destinations/", filename)
 
 
 class Destination(models.Model):
     name = models.CharField(max_length=63)
     country = models.CharField(max_length=63)
-    description = models.TextField()
-    review_number = models.IntegerField()
-    rating = models.IntegerField()
+    description = models.TextField(blank=True)
+    image = models.ImageField(null=True, upload_to=destination_image_file_path)
+    url_map_destination = models.URLField(max_length=255, blank=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name}, {self.country}"
+
+
+def stay_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/stays/", filename)
 
 
 class Stay(models.Model):
-    AMENITIES_CHOICES = [
-        ('wifi', 'Wi-Fi'),
-        ('parking', 'Parking'),
-        ('pool', 'Swimming Pool'),
-        ('balcony', 'Balcony'),
-        ('restaurant', 'Restaurant'),
-    ]
-
     name = models.CharField(max_length=63)
-    description = models.TextField()
-    amenities = models.CharField(max_length=20, choices=AMENITIES_CHOICES, blank=True)
+    description = models.TextField(blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    image = models.ImageField(null=True, upload_to=stay_image_file_path)
+    url_map_stay = models.URLField(max_length=255, blank=True)
+    destination = models.ForeignKey(
+        Destination, on_delete=models.CASCADE, related_name="stays"
+    )
+    amenities = models.ManyToManyField(Amenity, blank=True)
 
     def __str__(self):
         return self.name
 
 
+def stay_frames_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/stay_frames/", filename)
+
+
+class StayFrames(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(
+        upload_to=stay_frames_image_file_path, null=True
+    )
+    stays = models.ForeignKey(
+        Stay, on_delete=models.CASCADE, related_name="stay_frames"
+    )
+
+    class Meta:
+        ordering = ("title",)
+        verbose_name = "Stay frame"
+        verbose_name_plural = "Stay frames"
+
+    def __str__(self):
+        return self.title
+
+
+class RatingStar(models.Model):
+    value = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+
+    class Meta:
+        ordering = ("-value",)
+
+    def __str__(self):
+        return str(self.value)
+
+
+class RatingStay(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="user_rating_stay"
+    )
+    star = models.ForeignKey(
+        RatingStar, on_delete=models.CASCADE, related_name="star_rating_stay"
+    )
+    stay = models.ForeignKey(
+        Stay, on_delete=models.CASCADE, related_name="stay_rating"
+    )
+
+    def __str__(self):
+        return f"{self.star} - {self.stay}"
+
+
+class RatingDestination(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="user_rating_destination"
+    )
+    star = models.ForeignKey(
+        RatingStar, on_delete=models.CASCADE, related_name="star_rating_destination"
+    )
+    destination = models.ForeignKey(
+        Destination, on_delete=models.CASCADE, related_name="destination_rating"
+    )
+
+    def __str__(self):
+        return f"{self.star} - {self.destination}"
+
+
+class ReviewStay(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="user_review_stay"
+    )
+    text = models.TextField(blank=True)
+    stay = models.ForeignKey(
+        Stay, on_delete=models.CASCADE, related_name="review_stays"
+    )
+
+    def __str__(self):
+        return f"{self.user} - {self.stay}"
+
+
+class ReviewDestination(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="user_review_destination"
+    )
+    text = models.TextField(blank=True)
+    destination = models.ForeignKey(
+        Destination, on_delete=models.CASCADE, related_name="review_destinations"
+    )
+
+    def __str__(self):
+        return f"{self.user} - {self.destination}"
+
+
+def room_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/rooms/", filename)
+
+
+class Accommodation(models.Model):
+    TYPE_ROOM_CHOICES = (
+        ("STANDARD", "standard"),
+        ("JUNIOR SUITE", "junior suite"),
+        ("SUITE", "suite")
+    )
+
+    NUMBER_ROOMS_CHOICES = (
+        ("ONE ROOM", "one room"),
+        ("TWO ROOM", "two room"),
+        ("THREE ROOM", "three room"),
+        ("FOUR ROOM", "four room")
+    )
+
+    NUMBER_BEDS_CHOICES = (
+        ("SINGLE-BED", "single-bed"),
+        ("DOUBLE-BED", "double-bed"),
+        ("TRIPLE-BED", "triple-bed"),
+        ("FOUR-BED", "four-bed"),
+        ("FIVE-BED", "five-bed"),
+        ("SIX-BED", "six-bed"),
+        ("EIGHT-BED", "eight-bed")
+    )
+
+    name = models.CharField(max_length=255)
+    type_room = models.CharField(
+        max_length=255, choices=TYPE_ROOM_CHOICES, default="STANDARD"
+    )
+    number_rooms = models.CharField(
+        max_length=100, choices=NUMBER_ROOMS_CHOICES, default="ONE ROOM"
+    )
+    number_beds = models.CharField(
+        max_length=100, choices=NUMBER_BEDS_CHOICES, default="SINGLE-BED"
+    )
+    image = models.ImageField(null=True, upload_to=room_image_file_path)
+
+    def __str__(self):
+        return self.name
+
+
+def room_frames_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/room_frames/", filename)
+
+
+class AccommodationFrames(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(
+        upload_to=room_frames_image_file_path, null=True
+    )
+    rooms = models.ForeignKey(
+        Accommodation, on_delete=models.CASCADE, related_name="room_frames"
+    )
+
+    class Meta:
+        ordering = ("title",)
+        verbose_name = "Stay frame"
+        verbose_name_plural = "Stay frames"
+
+    def __str__(self):
+        return self.title
+
+
 class Booking(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="user_booking",
+    )
     arrival_date = models.DateField()
     departure_date = models.DateField()
     number_of_guests = models.IntegerField()
     night_price = models.DecimalField(max_digits=8, decimal_places=2)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
-    stay = models.ForeignKey(to=Stay, on_delete=models.CASCADE)
+    stay = models.ForeignKey(
+        Stay, on_delete=models.CASCADE, related_name="booking_stay"
+    )
+    rooms = models.ForeignKey(
+        Accommodation, on_delete=models.CASCADE, related_name="booking_room"
+    )
 
     def save(self, *args, **kwargs):
         self.calculate_total_price()
@@ -46,5 +258,4 @@ class Booking(models.Model):
         self.total_price = self.night_price * nights
 
     def __str__(self):
-        return f'Booking for {self.number_of_guests} guests'
-
+        return f"Booking for {self.number_of_guests} guests"
