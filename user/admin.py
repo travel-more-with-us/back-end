62695d56655c1531063b90e484e92e9e5823c896
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-06-29 17:52:59 +0300

    Changed User model & optimized admin.py for User model

--- user/admin.py
+++ user/admin.py
@@ -3,4 +3,26 @@ from django.contrib.auth.admin import UserAdmin
 
 from user.models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("phone_number", "residency", "status")
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("phone_number", "residency", "status")}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "phone_number",
+                        "residency",
+                        "status"
+                    )
+                },
+            ),
+        )
+    )
